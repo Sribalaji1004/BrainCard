b3d07aca2c75800d92704ef7c1c510ef1e24ce9e
Sribalaji1004 <sribalaji1004@outlook.com>
2016-09-26 16:45:58 +0530

    default

---
+++ app/Car.js
@@ -0,0 +1,12 @@
+"use strict";
+var Car = (function () {
+    function Car(id, name, power, alterEgo) {
+        this.id = id;
+        this.name = name;
+        this.power = power;
+        this.alterEgo = alterEgo;
+    }
+    return Car;
+}());
+exports.Car = Car;
+//# sourceMappingURL=Car.js.map
